a1d48c1380ab76bb822c8d219a1cb9469598de12
polas-cse <mdpolashossein@gmail.com>
2026-03-29 18:42:34 +0600

    Implementing Synchronous Callback Functions

---
+++ src/main/java/com/advance/learn/AdvanceLearnJavaApplication.java
@@ -0,0 +1,13 @@
+package com.advance.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvanceLearnJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvanceLearnJavaApplication.class, args);
+	}
+
+}

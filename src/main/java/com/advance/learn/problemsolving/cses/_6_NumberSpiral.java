7275a53ce7e5ce64cfd2e75a8e60fa8f565fc8cb
polas-cse <mdpolashossein@gmail.com>
2026-07-14 11:08:34 +0600

    Number Spiral

--- src/main/java/com/advance/learn/problemsolving/cses/_6_NumberSpiral.java
+++ src/main/java/com/advance/learn/problemsolving/cses/_6_NumberSpiral.java
@@ -12,14 +12,26 @@ public class _6_NumberSpiral {
 
         while (t-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
+            long row = Long.parseLong(st.nextToken());
+            long col = Long.parseLong(st.nextToken());
+            long k = Math.max(row, col);
             long ans;
             if (k % 2 == 1) {
+                if (col == k) {
+                    ans = k * k - row + 1;
+                } else {
+                    ans = (k - 1) * (k - 1) + col;
                 }
             } else {
+                if (row == k) {
+                    ans = k * k - col + 1;
+                } else {
+                    ans = (k - 1) * (k - 1) + row;
                 }
             }
             sb.append(ans).append('\n');
         }
+
         System.out.print(sb);
     }
 }
